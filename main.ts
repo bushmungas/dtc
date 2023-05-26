2fa98fc8bd04855a5f1cfca20316bb87df8cb7ef
bushmungas <125838027+bushmungas@users.noreply.github.com>
2023-05-26 12:09:24 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let item = 0
+input.onGesture(Gesture.Shake, function () {
+    item = randint(0, 2)
+    if (true) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
